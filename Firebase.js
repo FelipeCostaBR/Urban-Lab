123ed5c03bf10c64aa5421690aedb7e842079c56
Felipe <felipejsilvacosta@gmail.com>
2021-01-18 09:07:08 +1100

    chat

--- Firebase.js
+++ Firebase.js
@@ -1,23 +1,73 @@
-import firebase from "firebase"
+import firebase from "firebase";
 
 class Fire {
-    constructor(){
-        this.init()
-        this.checkAuth()
+  constructor() {
+    this.init();
+    this.checkAuth();
+  }
+
+  init = () => {
+    if (!firebase.apps.length) {
+      firebase.initializeApp({
+        apiKey: "AIzaSyBMhud-krbZCecV3oMXc0ei_oIDSzlmeDM",
+        authDomain: "teia-chat.firebaseapp.com",
+        databaseURL: "https://teia-chat-default-rtdb.firebaseio.com",
+        projectId: "teia-chat",
+        storageBucket: "teia-chat.appspot.com",
+        messagingSenderId: "934550895538",
+        appId: "1:934550895538:web:8ee7ca2526a51ea8cd02ab",
+        measurementId: "G-23J90VC870",
+      });
     }
 
-    init =() => {
-        if (!firebase.apps.length) {
-            firebase.initializeApp({
-                apiKey: "AIzaSyBMhud-krbZCecV3oMXc0ei_oIDSzlmeDM",
-                authDomain: "teia-chat.firebaseapp.com",
-                databaseURL: "https://teia-chat-default-rtdb.firebaseio.com",
-                projectId: "teia-chat",
-                storageBucket: "teia-chat.appspot.com",
-                messagingSenderId: "934550895538",
-                appId: "1:934550895538:web:8ee7ca2526a51ea8cd02ab",
-                measurementId: "G-23J90VC870"
-            })
+    checkAuth = () => {
+      firebase.auth().onAuthStateChanged((user) => {
+        if (!user) {
+          firebase.auth().sighInAnonymously();
         }
-    }
+      });
+    };
+  };
+  send = (messages) => {
+    messages.forEach((item) => {
+      const message = {
+        text: item.text,
+        timestamp: firebase.database.ServerValue.TIMESTAMP,
+        user: item.user,
+      };
+
+      this.db.push(message);
+    });
+  };
+
+  parse = (message) => {
+    const { user, text, timestamp } = message.val();
+    const { key: _id } = message;
+    const createdAt = new DataCue(timestamp);
+
+    return {
+      _id,
+      createdAt,
+      text,
+      user,
+    };
+  };
+
+  get = (callback) => {
+    this.db.on("child_added", (snapshot) => callback(this.parse(snapshot)));
+  };
+
+  off() {
+    this.db.off();
+  }
+
+  get db() {
+    return firebase.database().ref("message");
+  }
+
+  get db() {
+    return firebase.auth().currentUser || {}.uid;
+  }
 }
+
+export default new Fire();
--- src/pages/Chat/ChatScreen.js
+++ src/pages/Chat/ChatScreen.js
@@ -1,20 +1,54 @@
-import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import React, { Component } from "react";
+import { Platform, KeyboardAvoidingView, SafeAreaView } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 
+import Fire from "../../../Firebase";
 export default class ChatScreen extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text> Screen </Text>
-            </View>
-        )
-    }
-}
+  state = {
+    messages: [],
+  };
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
+  get user() {
+    return {
+      _id: Fire.uid,
+      name: this.props.navigation.state.params.name,
+    };
+  }
+
+  componentDidMount() {
+    Fire.get((message) =>
+      this.setState((previous) => ({
+        messages: GiftedChat.append(previous.messages, message),
+      }))
+    );
+  }
+
+  componentWillUnmount() {
+    Fire.off();
+  }
+
+  render() {
+    const chat = (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={Fire.send}
+        user={this.user}
+      />
+    );
+
+    if (Platform.OS === "android") {
+      return (
+        <KeyboardAvoidingView
+          style={{ flex: 1 }}
+          behavior="padding"
+          keyboardVerticalOffset={30}
+          enabled
+        >
+          {chat}
+        </KeyboardAvoidingView>
+      );
     }
-})
+
+    return <SafeAreaView style={{ flex: 1 }}>{chat}</SafeAreaView>;
+  }
+}
